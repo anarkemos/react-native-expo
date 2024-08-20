79cb5ec76a4fa32904560bd02bebf796c8117de7
anarkemos <anarkemos@gmail.com>
2024-08-20 14:18:04 -0500

    Los datos del register se guardan en arreglos

--- app/components/auth/loginComponent.tsx
+++ app/components/auth/loginComponent.tsx
@@ -1,67 +1,79 @@
-import React, { useState } from 'react';
-import { Text, View, Image, TouchableOpacity, Alert } from 'react-native';
-import { TextInput } from 'react-native-gesture-handler';
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import GlobalStyles from '../../styles/globalStyles';
+import React, { useState } from "react";
+import { Text, View, Image, TouchableOpacity, Alert } from "react-native";
+import { TextInput } from "react-native-gesture-handler";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import GlobalStyles from "../../styles/globalStyles";
 
 const LoginComponent = ({ navigation }) => {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-    const handleLogin = async () => {
-        try {
-            // Recuperar los datos del AsyncStorage
-            const storedEmail = await AsyncStorage.getItem('email');
-            const storedPassword = await AsyncStorage.getItem('password');
+  const handleLogin = async () => {
+    try {
+      // Recuperar los datos del AsyncStorage
+      const storedData = await AsyncStorage.getItem("RegisterData");
 
-            // Validar el inicio de sesión
-            if (storedEmail === email && storedPassword === password) {
-                // Navegar a la pantalla de inicio de sesión
-                navigation.navigate('dashboard');
-            } else {
-                // Mostrar un mensaje de error
-                window.alert('Error. Correo o contraseña incorrectos');
-            }
-        } catch (error) {
-            // Manejar cualquier error durante la recuperación de datos
-            window.alert('Error.Hubo un problema al intentar iniciar sesión');
+      if (storedData !==null){
+        const storedDataArray = JSON.parse(storedData);
+        const userFound = storedDataArray .find(
+            user=> user.email === email && user.password === password
+        );
+        if(userFound){
+            navigation.navigate('dashboard');
+        }else{
+            window.alert('no funkó')
         }
-    };
+      }
 
-    return (
-        <View style={GlobalStyles.container_auth}>
-            <Text style={GlobalStyles.title_auth}>Iniciar Sesión</Text>
-            <Image source={require('../../../assets/SenaLogo.png')} style={GlobalStyles.logo_auth} />
-            <TextInput
-                style={GlobalStyles.input_auth}
-                placeholder='Correo'
-                keyboardType='email-address'
-                autoCapitalize='none'
-                maxLength={25}
-                value={email}
-                onChangeText={setEmail}
-            />
-            <TextInput
-                style={GlobalStyles.input_auth}
-                placeholder='Contraseña'
-                maxLength={25}
-                secureTextEntry={true}
-                value={password}
-                onChangeText={setPassword}
-            />
-            <Text style={GlobalStyles.authText}>
-                ¿Olvidaste tu contraseña?
-                <Text style={GlobalStyles.textlink_auth}> Recuperar</Text>
-            </Text>
-            <Text style={GlobalStyles.authText}>
-                ¿No tienes cuenta?
-                <Text onPress={() => navigation.navigate('register')} style={GlobalStyles.textlink_auth}> Registrarme</Text>
-            </Text>
-            <TouchableOpacity onPress={handleLogin} style={GlobalStyles.auth_btn}>
-                <Text style={GlobalStyles.authText_btn}> Iniciar Sesión </Text>
-            </TouchableOpacity>
-        </View>
-    );
+    } catch (error) {
+      // Manejar cualquier error durante la recuperación de datos
+      window.alert("Error. Hubo un problema al intentar iniciar sesión");
+    }
+  };
+
+  return (
+    <View style={GlobalStyles.container_auth}>
+      <Text style={GlobalStyles.title_auth}>Iniciar Sesión</Text>
+      <Image
+        source={require("../../../assets/SenaLogo.png")}
+        style={GlobalStyles.logo_auth}
+      />
+      <TextInput
+        style={GlobalStyles.input_auth}
+        placeholder="Correo"
+        keyboardType="email-address"
+        autoCapitalize="none"
+        maxLength={25}
+        value={email}
+        onChangeText={setEmail}
+      />
+      <TextInput
+        style={GlobalStyles.input_auth}
+        placeholder="Contraseña"
+        maxLength={25}
+        secureTextEntry={true}
+        value={password}
+        onChangeText={setPassword}
+      />
+      <Text style={GlobalStyles.authText}>
+        ¿Olvidaste tu contraseña?
+        <Text style={GlobalStyles.textlink_auth}> Recuperar</Text>
+      </Text>
+      <Text style={GlobalStyles.authText}>
+        ¿No tienes cuenta?
+        <Text
+          onPress={() => navigation.navigate("register")}
+          style={GlobalStyles.textlink_auth}
+        >
+          {" "}
+          Registrarme
+        </Text>
+      </Text>
+      <TouchableOpacity onPress={handleLogin} style={GlobalStyles.auth_btn}>
+        <Text style={GlobalStyles.authText_btn}> Iniciar Sesión </Text>
+      </TouchableOpacity>
+    </View>
+  );
 };
 
 export default LoginComponent;
--- app/components/auth/registerComponent.tsx
+++ app/components/auth/registerComponent.tsx
@@ -1,89 +1,104 @@
-import React, { useState } from 'react';
-import { Text, View, TouchableOpacity, TextInput, Image, Alert } from 'react-native';
-import { ScrollView } from 'react-native-gesture-handler';
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import GlobalStyles from '../../styles/globalStyles';
+import React, { useState } from "react";
+import {
+  Text,
+  View,
+  TouchableOpacity,
+  TextInput,
+  Image,
+  Alert,
+} from "react-native";
+import { ScrollView } from "react-native-gesture-handler";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import GlobalStyles from "../../styles/globalStyles";
 
 const RegisterComponent = ({ navigation }) => {
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
-    const [confirmPassword, setConfirmPassword] = useState('');
-    const [email, setEmail] = useState('');
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [email, setEmail] = useState("");
 
-    const handleRegister = async () => {
-        if (!username || !email || !password || password !== confirmPassword) {
-            window.alert('Error. Por favor, complete todos los campos y asegúrese de que las contraseñas coincidan.');
-            return;
-        }
+  const handleRegister = async () => {
+    if (!username || !email || !password || password !== confirmPassword) {
+      window.alert(
+        "Error. Por favor, complete todos los campos y asegúrese de que las contraseñas coincidan."
+      );
+      return;
+    }
 
-        try {
-            // Guardar los datos en AsyncStorage
-            await AsyncStorage.setItem('email', email);
-            await AsyncStorage.setItem('password', password); // Sin JSON.stringify
-            await AsyncStorage.setItem('user', username); // Sin JSON.stringify
+    try {
+      let localStorage = [];
 
-            // Navegar a la pantalla de inicio de sesión
-            navigation.navigate('login');
-        } catch (error) {
-            window.alert('Error. Hubo un problema al registrar el usuario');
-        }
-    };
+      // Guardar los datos en AsyncStorage
+      localStorage.push({ username, email, password });
+      const localStorageToString = JSON.stringify(localStorage);
+      await AsyncStorage.setItem("RegisterData", localStorageToString);
 
-    return (
-        <ScrollView>
-            <View style={GlobalStyles.container_auth}>
-                <Text style={GlobalStyles.title_auth}>Registro</Text>
-                <Image source={require('../../../assets/SenaLogo.png')} style={GlobalStyles.logo_auth} />
-                <TextInput
-                    placeholder='Nombre Completo'
-                    maxLength={25}
-                    value={username}
-                    style={GlobalStyles.input_auth}
-                    onChangeText={setUsername}
-                />
-                <TextInput
-                    placeholder='Correo'
-                    keyboardType='email-address'
-                    autoCapitalize='none'
-                    maxLength={50}
-                    value={email}
-                    style={GlobalStyles.input_auth}
-                    onChangeText={setEmail}
-                />
-                <TextInput
-                    placeholder='Contraseña'
-                    maxLength={25}
-                    secureTextEntry={true}
-                    value={password}
-                    style={GlobalStyles.input_auth}
-                    onChangeText={setPassword}
-                />
-                <TextInput
-                    placeholder='Repetir Contraseña'
-                    maxLength={25}
-                    secureTextEntry={true}
-                    value={confirmPassword}
-                    style={GlobalStyles.input_auth}
-                    onChangeText={setConfirmPassword}
-                />
-                <Text style={GlobalStyles.authText}>
-                    Ya tengo cuenta
-                    <Text
-                        onPress={() => navigation.navigate('login')}
-                        style={GlobalStyles.textlink_auth}
-                    >
-                        {' '}Ingresar
-                    </Text>
-                </Text>
-                <TouchableOpacity
-                    onPress={handleRegister}
-                    style={GlobalStyles.auth_btn}
-                >
-                    <Text style={GlobalStyles.authText_btn}>Registrarme</Text>
-                </TouchableOpacity>
-            </View>
-        </ScrollView>
-    );
+      // Navegar a la pantalla de inicio de sesión
+      navigation.navigate("login");
+    } catch (error) {
+      window.alert("Error. Hubo un problema al registrar el usuario");
+    }
+  };
+
+  return (
+    <ScrollView>
+      <View style={GlobalStyles.container_auth}>
+        <Text style={GlobalStyles.title_auth}>Registro</Text>
+        <Image
+          source={require("../../../assets/SenaLogo.png")}
+          style={GlobalStyles.logo_auth}
+        />
+        <TextInput
+          placeholder="Nombre Completo"
+          maxLength={25}
+          value={username}
+          style={GlobalStyles.input_auth}
+          onChangeText={setUsername}
+        />
+        <TextInput
+          placeholder="Correo"
+          keyboardType="email-address"
+          autoCapitalize="none"
+          maxLength={50}
+          value={email}
+          style={GlobalStyles.input_auth}
+          onChangeText={setEmail}
+        />
+        <TextInput
+          placeholder="Contraseña"
+          maxLength={25}
+          secureTextEntry={true}
+          value={password}
+          style={GlobalStyles.input_auth}
+          onChangeText={setPassword}
+        />
+        <TextInput
+          placeholder="Repetir Contraseña"
+          maxLength={25}
+          secureTextEntry={true}
+          value={confirmPassword}
+          style={GlobalStyles.input_auth}
+          onChangeText={setConfirmPassword}
+        />
+        <Text style={GlobalStyles.authText}>
+          Ya tengo cuenta
+          <Text
+            onPress={() => navigation.navigate("login")}
+            style={GlobalStyles.textlink_auth}
+          >
+            {" "}
+            Ingresar
+          </Text>
+        </Text>
+        <TouchableOpacity
+          onPress={handleRegister}
+          style={GlobalStyles.auth_btn}
+        >
+          <Text style={GlobalStyles.authText_btn}>Registrarme</Text>
+        </TouchableOpacity>
+      </View>
+    </ScrollView>
+  );
 };
 
 export default RegisterComponent;
